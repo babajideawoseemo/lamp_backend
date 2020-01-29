c771524a7daf8eea1cd9b0b24f2e63351066ab73
akeens4 <akinloluolaposi@gmail.com>
2020-01-29 15:44:12 +0100

    first commit

---
+++ lamp_data/src/main/java/org/lamp_data/App.java
@@ -0,0 +1,13 @@
+package org.lamp_data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
